ae424d92c762157c87e93dee366c192b9ddf4912
Maxime Robert <maxime.robert1992@gmail.com>
2019-07-05 16:32:26 +0100

    feat: handle errors and display update results for all articles

---
+++ src/helpers.ts
@@ -0,0 +1,35 @@
+import { ArticlePublishedStatus, UpdateStatus } from './dev-to-git.interface';
+
+export const formatArticlePublishedStatuses = (articlePublishedStatuses: ArticlePublishedStatus[]): string => {
+  return articlePublishedStatuses
+    .map(articleStatus => {
+      if (articleStatus.updateStatus === UpdateStatus.FAILED_TO_EXTRACT_FRONT_MATTER) {
+        return `Article with ID "${articleStatus.articleId}" doesn't have a front matter correctly formatted`;
+      }
+
+      let text: string = `Article "${articleStatus.articleTitle}" `;
+
+      switch (articleStatus.updateStatus) {
+        case UpdateStatus.ALREADY_UP_TO_DATE:
+          text += `is already up to date`;
+          break;
+        case UpdateStatus.ERROR:
+          text += `encountered an error while updating`;
+          break;
+        case UpdateStatus.UPDATED:
+          text += `has been successfully updated`;
+          break;
+
+        default:
+          throw new UnreachabelCase(articleStatus.updateStatus);
+      }
+
+      return text;
+    })
+    .join(`\n`);
+};
+
+class UnreachabelCase {
+  // tslint:disable-next-line:no-empty
+  constructor(payload: never) {}
+}
