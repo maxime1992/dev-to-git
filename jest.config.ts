8654ab38546fab6cdbb42a6cd49befa234b9f0ff
Maxime Robert <maxime.robert1992@gmail.com>
2023-06-16 13:39:09 +0200

    test: fix broken jest setup

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { JestConfigWithTsJest } from 'ts-jest'
+
+const config: JestConfigWithTsJest = {
+  extensionsToTreatAsEsm: ['.ts'],
+  verbose: true,
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)?$': ['ts-jest', { useESM: true }]
+  },
+  testPathIgnorePatterns: ['./dist'],
+  moduleFileExtensions: ['ts', 'js', 'json']
+}
+
+export default config
