c0b674125dd9978751c778c29b8828a7eb61ca90
Maxime Robert <maxime.robert1992@gmail.com>
2023-06-16 13:42:53 +0200

    refactor: migrate the logic to launch the script to the index.js so that it's still possible to test dev-to-git.ts without launching the script

--- src/dev-to-git.ts
+++ src/dev-to-git.ts
@@ -120,27 +120,3 @@ export class DevToGit {
     return articlePublishedStatuses;
   }
 }
-
-// @todo move to main file?
-const devToGit = new DevToGit();
-devToGit
-  .publishArticles()
-  .then(articles => ({ articles, text: formatArticlePublishedStatuses(articles) }))
-  .then(res => {
-    devToGit.logger(res.text);
-
-    res.articles.forEach(article => {
-      if (
-        article.updateStatus === UpdateStatus.ERROR ||
-        article.updateStatus === UpdateStatus.FAILED_TO_EXTRACT_FRONT_MATTER
-      ) {
-        // if there's been at least one error, exit and fail
-        process.exit(1);
-      }
-    });
-  })
-  .catch(error => {
-    devToGit.logger(chalk.red(`An error occurred while publishing the articles`));
-    console.error(error);
-    process.exit(1);
-  });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node
 
 require('./dev-to-git.umd.js');
+
+const devToGit = new DevToGit();
+
+devToGit
+  .publishArticles()
+  .then(articles => ({ articles, text: formatArticlePublishedStatuses(articles) }))
+  .then(res => {
+    devToGit.logger(res.text);
+
+    res.articles.forEach(article => {
+      if (
+        article.updateStatus === UpdateStatus.ERROR ||
+        article.updateStatus === UpdateStatus.FAILED_TO_EXTRACT_FRONT_MATTER
+      ) {
+        // if there's been at least one error, exit and fail
+        process.exit(1);
+      }
+    });
+  })
+  .catch(error => {
+    devToGit.logger(chalk.red(`An error occurred while publishing the articles`));
+    console.error(error);
+    process.exit(1);
+  });
