173bddead59dc943ab27c76e12da47f3bff24b66
Maxime Robert <maxime.robert1992@gmail.com>
2023-06-18 08:47:48 +0200

    chore: add a comment on getArticles to explain some of the logic with the API

--- src/data/dev-to/articles/articles.service.ts
+++ src/data/dev-to/articles/articles.service.ts
@@ -9,6 +9,10 @@ export class ArticlesService {
 
   constructor(private httpService: HttpService) {}
 
+  // dev.to API returns a maximum of 1000 articles but would by default return only 30
+  // https://docs.dev.to/api/#tag/articles/paths/~1articles~1me~1all/get
+  // instead of having to manage the pagination I think it's safe to assume people using
+  // dev-to-git won't have more than 1000 articles for now
   public getArticles(): Observable<ArticleApiResponse[]> {
     return this.httpService
       .get<ArticleApiResponse[]>(`${this.baseUrl}/me/all?per_page=1000`)
