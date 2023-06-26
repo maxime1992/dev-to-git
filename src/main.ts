27cc2490cf88a18bc161a1a2f9144445257ded52
Maxime Robert <maxime.robert1992@gmail.com>
2023-06-26 22:30:24 +0200

    fix: add env node comment in main to allow running it as global command

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
+// the above is needed as we publish this as a global package
+// https://stackoverflow.com/a/34354713/2398593
+
 import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 
