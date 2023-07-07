0bea626c2046b5146bbaa75a6f5ab0871155deeb
Maxime Robert <maxime.robert1992@gmail.com>
2023-07-07 08:43:10 +0200

    refactor: move the dev-to interceptor to a middleware folder

--- src/middlewares/dev-to.interceptor.spec.ts
+++ src/middlewares/dev-to.interceptor.spec.ts
@@ -2,19 +2,19 @@ import { AxiosFulfilledInterceptor } from '@narando/nest-axios-interceptor';
 import { HttpService } from '@nestjs/axios';
 import { Test, TestingModule } from '@nestjs/testing';
 import type { AxiosRequestConfig } from 'axios';
+import { DEV_TO_TOKEN } from '../data/dev-to/dev-to.tokens';
 import { DevToInterceptor } from './dev-to.interceptor';
-import { DEV_TO_TOKEN } from './dev-to.tokens';
 
 describe('DevTo interceptor', () => {
   let service: DevToInterceptor;
 
-  const MOCK_DEV_TO_TOKEN = 'mock-token'
+  const MOCK_DEV_TO_TOKEN = 'mock-token';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         { provide: HttpService, useValue: jest.fn() },
-        { provide: DEV_TO_TOKEN, useValue:  MOCK_DEV_TO_TOKEN},
+        { provide: DEV_TO_TOKEN, useValue: MOCK_DEV_TO_TOKEN },
         DevToInterceptor,
       ],
     }).compile();
@@ -26,16 +26,27 @@ describe('DevTo interceptor', () => {
     let configCallback: AxiosFulfilledInterceptor<AxiosRequestConfig>;
 
     beforeEach(() => {
-      configCallback =   service.requestFulfilled();
+      configCallback = service.requestFulfilled();
     });
 
     it(`should not add any api-key header if the request isn't for dev.to API`, async () => {
-      expect((await configCallback({baseURL:'http://test.com'})).headers?.['api-key']).toBeUndefined()
+      expect(
+        (await configCallback({ baseURL: 'http://test.com' })).headers?.[
+          'api-key'
+        ],
+      ).toBeUndefined();
     });
 
     it(`should add an api-key header if the request is for dev.to API`, async () => {
-      expect((await configCallback({baseURL:'https://dev.to/api'})).headers['api-key']).toBe(MOCK_DEV_TO_TOKEN)
-      expect((await configCallback({baseURL:'https://dev.to/api/anything/else'})).headers['api-key']).toBe(MOCK_DEV_TO_TOKEN)
+      expect(
+        (await configCallback({ baseURL: 'https://dev.to/api' })).headers[
+          'api-key'
+        ],
+      ).toBe(MOCK_DEV_TO_TOKEN);
+      expect(
+        (await configCallback({ baseURL: 'https://dev.to/api/anything/else' }))
+          .headers['api-key'],
+      ).toBe(MOCK_DEV_TO_TOKEN);
     });
   });
 });
